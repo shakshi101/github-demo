703cdef13525a1604b34038e0cb93b6ae96bc372
Shakshi-Sharma <[9576shakshi@gmail.com]>
2024-03-07 12:03:41 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,22 @@
+#name1 = "tony"
+#name2 = "stark"
+#sum =name1+name2
+#print(sum)
+#age =52
+#print(age)
+#name=input("name")
+#print("hello "+name)
+#print(name.upper())
+#print(name.lower())
+#print(name.find("s"))
+#print(name.replace("s","a"))
+num1= 50
+if num1%10==0 and num1%50==0:
+    if num1%30==0:
+        print("no. is div by 10 ,50 ,30")
+    else:
+            print("no. div by 10 and 50")
+        else:
+            print("no. not divisible")
+     
+            
